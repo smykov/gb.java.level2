699493453e7d6a6d6a04598380bde144b6d09969
Konstantin <smykovkn@gmail.com>
2022-06-02 23:21:11 +0700

    refactored task 3

--- src/main/java/ru/gb/smykov/level2/hw2/App.java
+++ src/main/java/ru/gb/smykov/level2/hw2/App.java
@@ -11,27 +11,44 @@ public class App {
         };
 
         try {
-            System.out.println(checkArray4x4(strings));
+            System.out.println(getSum(strings));
         } catch (MyArraySizeException | MyArrayDataException e) {
             e.printStackTrace();
         }
     }
 
-    private static int checkArray4x4(String[][] strings) {
+    private static void checkArray4x4(String[][] strings) throws MyArrayDataException {
         if (strings.length != 4 || strings[0].length != 4) {
             throw new MyArraySizeException();
         }
+    }
+
+    private static int getSum(String[][] strings) {
+        checkArray4x4(strings);
 
         int sum = 0;
         for (int i = 0; i < strings.length; i++) {
             for (int j = 0; j < strings[i].length; j++) {
+                int integer = 0;
                 try {
-                    sum += Integer.parseInt(strings[i][j].trim());
-                } catch (NumberFormatException e) {
-                    throw new MyArrayDataException(i, j);
+                    integer = getInteger(strings, i, j);
+                } catch (MyArrayDataException e) {
+                    e.printStackTrace();
+                } finally {
+                    sum += integer;
                 }
             }
         }
         return sum;
+    }
+
+    private static int getInteger(String[][] strings, int i, int j) throws MyArrayDataException {
+        int integer;
+        try {
+            integer = Integer.parseInt(strings[i][j].trim());
+        } catch (NumberFormatException e) {
+            throw new MyArrayDataException(i, j);
+        }
+        return integer;
     }
 }
